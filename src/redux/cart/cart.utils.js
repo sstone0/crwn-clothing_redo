95ceadd79b80aee88c48c86ffeaccc159de2ee2d
sstone0 <shawnrstone1995@gmail.com>
2021-11-07 16:10:09 -0500

    updating shop data urls

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -19,12 +19,12 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
     (cartItem) => cartItem.id === cartItemToRemove.id
   );
 
-  if (existingCartItem.quantity == 1) {
+  if (existingCartItem.quantity === 1) {
     return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
   }
 
   return cartItems.map((cartItem) =>
-    cartItem.id == cartItemToRemove.id
+    cartItem.id === cartItemToRemove.id
       ? { ...cartItem, quantity: cartItem.quantity - 1 }
       : cartItem
   );
