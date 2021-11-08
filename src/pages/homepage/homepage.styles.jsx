b61beaf104e9f111991b99f1bc61486ffc571654
sstone0 <shawnrstone1995@gmail.com>
2021-11-07 20:25:38 -0500

    updated styled components for custom button and header

---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HomePageContainer = styled.div`
+  .homepage {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
