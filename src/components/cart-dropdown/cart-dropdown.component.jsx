db778889ef164e677c55b00e6db0c34e87024efd
sstone0 <shawnrstone1995@gmail.com>
2021-12-01 16:09:19 -0500

    update links

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,21 +11,22 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
-  <div className='cart-dropdown'>
-    <div className='cart-items'>
+  <div className="cart-dropdown">
+    <div className="cart-items">
       {cartItems.length ? (
         cartItems.map((cartItem) => (
           <CartItem key={cartItem.id} item={cartItem} />
         ))
       ) : (
-        <span className='empty-message'>Your cart is empty</span>
+        <span className="empty-message">Your cart is empty</span>
       )}
     </div>
     <CustomButton
       onClick={() => {
-        history.push("/checkout");
+        history.push("/crwn-clothing_redo/checkout");
         dispatch(toggleCartHidden());
-      }}>
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
