a7ed95ec3a52174936b487dd8dfaf8dd6dc70606
sstone0 <shawnrstone1995@gmail.com>
2022-02-10 08:39:40 -0500

    removed additional imports

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from 'react';
 
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
-import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import "./cart-icon.styles.scss";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import './cart-icon.styles.scss';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const CartIcon = () => {
-  const dispatch = useDispatch();
-  const toggleCartHiddenClickHandler = () => dispatch(toggleCartHidden());
-  const itemCount = useSelector(selectCartItemsCount);
+	const dispatch = useDispatch();
+	const toggleCartHiddenClickHandler = () => dispatch(toggleCartHidden());
+	const itemCount = useSelector(selectCartItemsCount);
 
-  return (
-    <div className='cart-icon' onClick={toggleCartHiddenClickHandler}>
-      <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>{itemCount}</span>
-    </div>
-  );
+	return (
+		<div className="cart-icon" onClick={toggleCartHiddenClickHandler}>
+			<ShoppingIcon className="shopping-icon" />
+			<span className="item-count">{itemCount}</span>
+		</div>
+	);
 };
 
 export default CartIcon;
